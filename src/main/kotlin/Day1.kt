1bf36c5461b0e7ebd078f6568d6268ce2ab75c9d
zsoki <tegyukfel@gmail.com>
2022-12-08 21:44:00 +0100

    Initial commit + Day 1

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,45 @@
+import java.io.File
+import java.util.*
+import kotlin.math.max
+
+/**
+ * https://adventofcode.com/2022/day/1
+ * --- Day 1: Calorie Counting ---
+ */
+
+fun day1Part1(inputFileName: String): Int {
+    var currentElfCalories = 0
+    var maxCalories = 0
+
+    File(ClassLoader.getSystemResource(inputFileName).file).forEachLine {
+        if (it.isBlank()) {
+            maxCalories = max(currentElfCalories, maxCalories)
+            currentElfCalories = 0
+        } else {
+            currentElfCalories += Integer.parseInt(it)
+        }
+    }
+
+    return maxCalories
+}
+
+fun day1Part2(inputFileName: String): Int {
+    var currentElfCalories = 0
+    val top3Calories = LinkedList(listOf(0, 0, 0))
+
+    File(ClassLoader.getSystemResource(inputFileName).file).forEachLine { line ->
+        if (line.isBlank()) {
+            for(idx in 0 until 3) {
+                if (currentElfCalories > top3Calories[idx]) {
+                    top3Calories.add(idx, currentElfCalories)
+                    break
+                }
+            }
+            currentElfCalories = 0
+        } else {
+            currentElfCalories += Integer.parseInt(line)
+        }
+    }
+
+    return top3Calories.take(3).toIntArray().sum()
+}
---
+++ src/test/kotlin/Day1Test.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test
+
+class Day1Test {
+
+    @Test
+    fun part1() {
+        println(day1Part1("day1.txt"))
+    }
+
+    @Test
+    fun part2() {
+        println(day1Part2("day1.txt"))
+    }
+
+}
